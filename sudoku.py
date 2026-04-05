7dcc27beae4145f53796fe148725cb7c5ffc0709
sakshithgoud7-droid <sakshithgoud7@gmail.com>
2026-04-05 11:58:09 +0530

    Create sudoku.py

---
+++ sudoku.py
@@ -0,0 +1,71 @@
+def print_board(board):
+    for i in range(9):
+        for j in range(9):
+            print(board[i][j], end=" ")
+        print()
+
+
+def find_empty(board):
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == 0:
+                return i, j
+    return None
+
+
+def is_valid(board, row, col, num):
+    if num in board[row]:
+        return False
+
+    for i in range(9):
+        if board[i][col] == num:
+            return False
+
+    start_row = (row // 3) * 3
+    start_col = (col // 3) * 3
+
+    for i in range(start_row, start_row + 3):
+        for j in range(start_col, start_col + 3):
+            if board[i][j] == num:
+                return False
+
+    return True
+
+
+def solve_sudoku(board):
+    empty = find_empty(board)
+
+    if not empty:
+        return True 
+
+    row, col = empty
+
+    for num in range(1, 10):
+        if is_valid(board, row, col, num):
+            board[row][col] = num
+
+            if solve_sudoku(board):
+                return True
+
+            board[row][col] = 0 
+
+    return False
+
+
+board = [
+    [0,0,3,0,2,0,6,0,0],
+    [9,0,0,3,0,5,0,0,1],
+    [0,0,1,8,0,6,4,0,0],
+    [0,0,8,1,0,2,9,0,0],
+    [7,0,0,0,0,0,0,0,8],
+    [0,0,6,7,0,8,2,0,0],
+    [0,0,2,6,0,9,5,0,0],
+    [8,0,0,2,0,3,0,0,9],
+    [0,0,5,0,1,0,3,0,0]
+]
+
+if solve_sudoku(board):
+    print("Solved Sudoku:\n")
+    print_board(board)
+else:
+    print("No solution exists")
